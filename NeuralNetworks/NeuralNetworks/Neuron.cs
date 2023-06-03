d05e4a478d1ad187c2890b7d1383e127128ebd73
Anastasiia Lopata <lopata.ad@gmail.com>
2023-06-03 16:39:33 +0200

    created progect

---
+++ NeuralNetworks/NeuralNetworks/Neuron.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworks
+{
+    class Neuron
+     {
+        public List<double> Weight { get; }
+        public NeuronType Type { get; }
+        public double Output { get; private set; }
+    }
+}
---
+++ NeuralNetworks/NeuralNetworks/NeuronType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworks
+{
+    enum NeuronType
+    { Input=0,
+        Normal=1,
+        Output=2
+
+    }
+}
