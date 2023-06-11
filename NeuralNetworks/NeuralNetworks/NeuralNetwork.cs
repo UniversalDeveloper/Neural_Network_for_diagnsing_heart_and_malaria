0df57caa3e3c82ea4a836f32863782b354ceea54
Anastasiia Lopata <lopata.ad@gmail.com>
2023-06-11 20:54:05 +0200

    created class Topologe og NeuralNetwor which in funure will be extention

---
+++ NeuralNetworks/NeuralNetworks/NeuralNetwork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworks
+{
+   public class NeuralNetwork
+    {
+        public List<Layers> Layers { get; }
+    }
+}
